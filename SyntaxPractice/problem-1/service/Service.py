ba017083be47a09c16273ac06048846afcd37d55
Karani Vinay Kumar <82294343+kvinay7@users.noreply.github.com>
2026-06-15 16:35:35 +0530

    Update Service.py

--- SyntaxPractice/problem-1/service/Service.py
+++ SyntaxPractice/problem-1/service/Service.py
@@ -1,1 +1,23 @@
+from model.Result import Result
+from exception.InputValidationException import InputValidationException
 
+
+class Service:
+
+    def process(self, numbers):
+
+        if numbers is None or len(numbers) == 0:
+            raise InputValidationException(
+                "Input list cannot be empty"
+            )
+
+        total = sum(numbers)
+
+        average = (
+            total / len(numbers)
+        )
+
+        return Result(
+            total,
+            average
+        )

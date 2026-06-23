d256c25afebad918d899acf1a76ed5eededd8251
Your Name <karani.vinaykumar7@gmail.com>
2026-06-23 09:32:16 +0530

    Add missing import for Min validation annotation in Employee model

--- DesignPractice/EmployeeManagementSystem/src/main/java/com/karan/employeemanagementsystem/models/Employee.java
+++ DesignPractice/EmployeeManagementSystem/src/main/java/com/karan/employeemanagementsystem/models/Employee.java
@@ -14,6 +14,7 @@ import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
